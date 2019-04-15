b2534225b6732b4444c9a8a9971da29c39b8a144
zhanglei29 <zhanglei29@le.com>
2019-04-15 17:48:28 +0800

    添加访问路径的注释

--- src/main/java/com/zl/controller/HelloWorldController.java
+++ src/main/java/com/zl/controller/HelloWorldController.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    //访问路径 localhost:8080/hello
     @RequestMapping("/hello")
     public Map index() {
-    	HashMap<String, Object> hashMap = new HashMap<>();
+        HashMap<String, Object> hashMap = new HashMap<>();
     	hashMap.put("userName", "张三");
     	hashMap.put("userAge", 12);
         return hashMap;
